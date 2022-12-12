420cdf054834fb433b77d907df9cddef04788646
MiguelFernandes13 <miguelffernandes13@gmail.com>
2022-12-12 18:03:14 +0000

    remove lock from bestNeighboor

--- NodeDatabase.py
+++ NodeDatabase.py
@@ -127,69 +127,62 @@ class NodeDataBase:
         jumpThreshold = 0.95
         # Iniciliazar o melhor viznho com os valores dos antigo melhor vizinho
         # Para evitar a troca de viznhos por melhorias insignificantes nas métricas
-        try:
-            self.lock.acquire()
 
-            if self.oldBest[0] == "":
-                bestNeighborStreaming = ("", "", sys.float_info.max,
-                                         sys.float_info.max)
-            else:
-                bestNeighborStreaming = (
-                    self.oldBest[0], self.oldBest[1],
-                    self.times[self.oldBest[0]][self.oldBest[1]],
-                    self.jumps[self.oldBest[0]][self.oldBest[1]]
-                )  # (ip, sever, time, jumps)
-            bestNeighborTime = bestNeighborStreaming
-
-            neighborStreaming = []
-            for neighbor in self.neighbors:
-                if self.streams.get(neighbor):
-                    neighborStreaming.append(neighbor)
-            # Percorrer os vizinhos e verificar se algum deles está a fazer stream
-            # Se estiver, guardar o vizinho com o melhores métricas, isto é, melhor tempo e menor número de saltos
-            if len(neighborStreaming) != 0:
-                for neighbor in neighborStreaming:
-                    for server in self.times[neighbor].keys():
-                        if self.times[neighbor][server] < bestNeighborStreaming[2]:
-                            #Se a diferença for minima o desempate é feito pelo número de saltos
-                            if self.times[neighbor][server] / bestNeighborStreaming[
-                                    2] > jumpThreshold:
-                                if self.jumps[neighbor][
-                                        server] < bestNeighborStreaming[3]:
-                                    bestNeighborStreaming = (
-                                        neighbor, server,
-                                        self.times[neighbor][server],
-                                        self.jumps[neighbor][server])
-                            else:
+        if self.oldBest[0] == "":
+            bestNeighborStreaming = ("", "", sys.float_info.max,
+                                     sys.float_info.max)
+        else:
+            bestNeighborStreaming = (
+                self.oldBest[0], self.oldBest[1],
+                self.times[self.oldBest[0]][self.oldBest[1]],
+                self.jumps[self.oldBest[0]][self.oldBest[1]]
+            )  # (ip, sever, time, jumps)
+        bestNeighborTime = bestNeighborStreaming
+        neighborStreaming = []
+        for neighbor in self.neighbors:
+            if self.streams.get(neighbor):
+                neighborStreaming.append(neighbor)
+        # Percorrer os vizinhos e verificar se algum deles está a fazer stream
+        # Se estiver, guardar o vizinho com o melhores métricas, isto é, melhor tempo e menor número de saltos
+        if len(neighborStreaming) != 0:
+            for neighbor in neighborStreaming:
+                for server in self.times[neighbor].keys():
+                    if self.times[neighbor][server] < bestNeighborStreaming[2]:
+                        #Se a diferença for minima o desempate é feito pelo número de saltos
+                        if self.times[neighbor][server] / bestNeighborStreaming[
+                                2] > jumpThreshold:
+                            if self.jumps[neighbor][
+                                    server] < bestNeighborStreaming[3]:
                                 bestNeighborStreaming = (
-                                    neighbor, server, self.times[neighbor][server],
+                                    neighbor, server,
+                                    self.times[neighbor][server],
                                     self.jumps[neighbor][server])
-
-            #Verificar qual o vizinho com o melhor tempo e menor número de saltos
-            for neighbor in self.neighbors:
-                if self.times.get(neighbor):
-                    for server in self.times.get(neighbor).keys():
-                        if self.times[neighbor][server] < bestNeighborTime[2]:
-                            if self.times[neighbor][server] / bestNeighborTime[
-                                    2] > jumpThreshold:
-                                if self.jumps[neighbor][server] < bestNeighborTime[
-                                        3]:
-                                    bestNeighborTime = (
-                                        neighbor, server,
-                                        self.times[neighbor][server],
-                                        self.jumps[neighbor][server])
-                            else:
-                                bestNeighborTime = (neighbor, server,
-                                                    self.times[neighbor][server],
-                                                    self.jumps[neighbor][server])
-
-            #Caso exista um vizinho que esteja streaming, esse é sempre o melhor vizinho,
-            #a menos que exista um vizinho que não esta a fazer stream com um tempo razoavelmente melhor
-            self.oldBest = bestNeighborTime
-            if bestNeighborStreaming[0] != "" and bestNeighborTime[ 
-                    2] / bestNeighborStreaming[2] > 0.7:
-                self.oldBest = bestNeighborStreaming
-        finally:
-            self.lock.release()
+                        else:
+                            bestNeighborStreaming = (
+                                neighbor, server, self.times[neighbor][server],
+                                self.jumps[neighbor][server])
+        #Verificar qual o vizinho com o melhor tempo e menor número de saltos
+        for neighbor in self.neighbors:
+            if self.times.get(neighbor):
+                for server in self.times.get(neighbor).keys():
+                    if self.times[neighbor][server] < bestNeighborTime[2]:
+                        if self.times[neighbor][server] / bestNeighborTime[
+                                2] > jumpThreshold:
+                            if self.jumps[neighbor][server] < bestNeighborTime[
+                                    3]:
+                                bestNeighborTime = (
+                                    neighbor, server,
+                                    self.times[neighbor][server],
+                                    self.jumps[neighbor][server])
+                        else:
+                            bestNeighborTime = (neighbor, server,
+                                                self.times[neighbor][server],
+                                                self.jumps[neighbor][server])
+        #Caso exista um vizinho que esteja streaming, esse é sempre o melhor vizinho,
+        #a menos que exista um vizinho que não esta a fazer stream com um tempo razoavelmente melhor
+        self.oldBest = bestNeighborTime
+        if bestNeighborStreaming[0] != "" and bestNeighborTime[ 
+                2] / bestNeighborStreaming[2] > 0.7:
+            self.oldBest = bestNeighborStreaming
 
         return self.oldBest[0]
