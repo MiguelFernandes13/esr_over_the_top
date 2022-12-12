ad9a83778d7d6930f8808de2f711f008a7f9e4a8
MiguelFernandes13 <miguelffernandes13@gmail.com>
2022-12-12 11:13:09 +0000

    streaming

--- NodeClient.py
+++ NodeClient.py
@@ -71,8 +71,7 @@ class NodeClient:
         print("Tempos de vida ", self.db.times)
         print("Saltos ", self.db.jumps)
         self.db.addSent(server_address, add[0], seq)
-        if (seq-1) in self.db.alreadySent[server_address] and len(self.db.getSendToBySeq(server_address, seq)) == len(self.db.getSendToBySeq(server_address, seq - 1)):
-            threading.Thread(target=self.recalculate_roots).start()
+        threading.Thread(target=self.recalculate_roots).start()
         # enviar para os vizinhos um keepalive com o tempo atual e o numero de saltos atualizado
         # enviar tambem se o nodo esta a fazer streaming
         for i in self.db.getNeighbors():
@@ -119,7 +118,6 @@ class NodeClient:
                 target=self.resend_stream, args=(self.db.getIpToInterface(best), ))
             p.start()
             self.db.processReceive = p
-            self.db.streaming = True
 
     def waitToStream(self, interface: str):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -173,6 +171,7 @@ class NodeClient:
         try:
             while True:
                 message, add = s.recvfrom(20480)
+                self.db.streaming = True
                 #print(f"Received stream from {add} and waitIp is {self.db.waitIp}")
                 if add[0] == self.db.waitIp:
                     self.db.waitStream.acquire()
