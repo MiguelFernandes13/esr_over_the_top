d2bf2a2db4ec985ce5ae32cadd6bb0e6f0c85a50
MiguelFernandes13 <miguelffernandes13@gmail.com>
2022-12-15 20:18:42 +0000

    Comentario no Database

--- Database.py
+++ Database.py
@@ -2,29 +2,33 @@ import threading
 import time
 import socket
 
+#Classe que guarda a informação de servidor alternativo
 class HelperServer:
-    ip: str
-    neighbors: list
+    ip: str # ip do servidor
+    neighbors: list # lista de ips dos vizinhos
 
     def __init__(self, ip, neighbors):
         self.ip = ip
         self.neighbors = neighbors
 
+    #Retorna o ip do servidor
     def getIp(self):
         return self.ip
 
+    #Retorna a lista de vizinhos
     def getNeighbors(self):
         return self.neighbors
 
+#Classe que guarda a informação de um nó
 class Node:
-    ip_to_server: str
-    internalInterfaces: list
-    clients: list
-    neighbors: list  # Node
-    isActive: bool
-    isStreaming: bool
-    port: int
-    rtpSocket: socket
+    ip_to_server: str # interface que comunica com o servidor
+    internalInterfaces: list # lista de interfaces internas
+    clients: list #lista de ips dos clientes
+    neighbors: list  # lista de ips dos vizinhos
+    isActive: bool # se está ativo
+    isStreaming: bool # se está a fazer stream
+    port: int 
+    rtpSocket: socket # socket RTP para fazer stream
 
     def __init__(self, internalInterfaces, clients, neighbors) -> None:
         self.ip_to_server = ""
@@ -34,37 +38,43 @@ class Node:
         self.isActive = False
         self.isStreaming = False
 
+    #Começa a stream num nó e guarda o socket RTP
     def startStreaming(self, s: socket):
         self.isStreaming = True
         self.rtpSocket = s
 
+    #Para a stream num nó
     def stopStreaming(self):
         self.isStreaming = False
 
+    #Ativa o nó
     def connect(self, ip):
         self.isActive = True
         self.ip_to_server = ip
 
+    #Desativa o nó
     def disconnect(self):
         self.isActive = False
 
+    #retorna se está ativo
     def active(self):
         return self.isActive
 
+    #retorna se o clientIp é um cliente do nó
     def isClient(self, clientIp):
         return clientIp in self.clients
 
 
 class Database:
-    ip: int
-    lock: threading.Lock
-    nodes: dict  # { 'interface' : Node}
-    all_nodes: list
-    neighbors: dict  # [ips]
-    streamTo: list  # [Node]
-    helperServers: list  # [HelperServer]
-    frameNumber: int
-    clientPort : dict # {ip : port}
+    ip: int # ip do servidor
+    lock: threading.Lock # lock para garantir que as variáveis são acessadas de forma segura
+    nodes: dict  # { 'interface' : Node} 
+    all_nodes: list # lista de todos os nós
+    neighbors: dict  # { 'interface' : nó }
+    streamTo: list  # [Node] lista de nós para onde se está a fazer stream
+    helperServers: list  # [HelperServer] lista de servidores alternativos
+    frameNumber: int # número do frame
+    clientPort : dict # {ip : port} dicionário que guarda a porta de cada cliente
 
     def __init__(self, Ip):
         self.ip = Ip
@@ -78,9 +88,11 @@ class Database:
         self.frameNumber = 0
         self.clientPort = {}
 
+    #Retorna a porta de um cliente
     def getClientPort(self, ip):
         return self.clientPort[ip]
 
+    #Adiciona a porta a um cliente
     def addClientPort(self, ip, port):
         try:
             self.lock.acquire()
@@ -88,6 +100,7 @@ class Database:
         finally:
             self.lock.release()
 
+    #Adiciona um servidor alternativo
     def addHelperServer(self, ip, neighbors):
         try:
             self.lock.acquire()
@@ -95,12 +108,14 @@ class Database:
         finally:
             self.lock.release()
 
+    #Retorna o servidor alternativo com base np ip dado
     def getHelperServer(self, ip):
         for helper in self.helperServers:
             if helper.getIp() == ip:
                 return helper
         return None
 
+    #Adiciona vizinhos ao servidor
     def addNeighbors(self, neighbors: list):
         try:
             self.lock.acquire()
@@ -111,6 +126,7 @@ class Database:
         finally:
             self.lock.release()
 
+    #Adiciona um nodo ao servidor
     def addNode(self, internalInterfaces, clients, neighbors):
         try:
             self.lock.acquire()
@@ -122,9 +138,11 @@ class Database:
         finally:
             self.lock.release()
 
+    #Retorna um nó com base no ip dado
     def getNode(self, nodeIp) -> Node:
         return self.nodes.get(nodeIp)
 
+    #Ativa um nó
     def connectNode(self, nodeIp):
         try:
             self.lock.acquire()
@@ -134,6 +152,7 @@ class Database:
         finally:
             self.lock.release()
 
+    #Desativa um nó
     def disconnectNode(self, nodeIp):
         try:
             self.lock.acquire()
@@ -143,6 +162,7 @@ class Database:
         finally:
             self.lock.release()
 
+    #Adiciona um nó à lista de nós para onde se está a fazer stream
     def joinStream(self, nodeIp, s):
         try:
             self.lock.acquire()
@@ -154,6 +174,7 @@ class Database:
         finally:
             self.lock.release()
 
+    #Remove um nó da lista de nós para onde se está a fazer stream
     def leaveStream(self, nodeIp):
         try:
             self.lock.acquire()
@@ -165,9 +186,11 @@ class Database:
         finally:
             self.lock.release()
 
+    #Retorna a lista de nós para onde se está a fazer stream
     def getStreamToList(self) -> list:
         return self.streamTo
 
+    #Retorna os vizinhos de um nó
     def getNeighbors(self, nodeIp) -> list:
         try:
             self.lock.acquire()
@@ -177,6 +200,7 @@ class Database:
         finally:
             self.lock.release()
 
+    #Retorna as interfaces internas de um nó
     def getInternalInterfaces(self, nodeIp) -> list:
         try:
             self.lock.acquire()
@@ -186,6 +210,7 @@ class Database:
         finally:
             self.lock.release()
 
+    #Retorna o nó conectado a um dado cliente
     def getStreamTo(self, clientIp) -> str:
         node: Node
         for node in self.all_nodes:
